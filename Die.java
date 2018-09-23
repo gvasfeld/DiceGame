0fc92faed98926b844db7abd6c881c0be1160e06
Greg Vasfeld <gvasfeld@regmail.west.com>
2018-09-23 13:25:42 -0500

    1.0

---
+++ Die.java
@@ -0,0 +1,62 @@
+
+/**
+ * Write a description of class Die here.
+ *
+ * @author (Mark Vasfeld)
+ * @version (1.0, Sep 23. 2018)
+ */
+public class Die
+{
+    //Declare private to hold face value of the dice
+    private int value;
+    private String name;
+
+    /**
+     * Constructor for objects of class Die
+     */
+    public Die()
+    {
+        // Call roll method to set the face value of the dice
+        roll();
+    }
+
+    /**
+     * Constructor for objects of class Die
+     * @param  String to hold the name of the die
+     */
+    public Die(String Name)
+    {
+        // Call roll method to set the face value of the dice
+        roll();
+        name=Name;
+    }
+    
+    /**
+     * Method sets the face falue of the dice using random number generator
+     * Face values could be: 1,2,3,4,5,6
+     * @param  no params
+     * @return no return value
+     */
+    public void roll()
+    {
+        value = (int)(Math.random()*6+1);
+    }
+    /**
+     * Method returns the face value of the dice
+     * @param  no params
+     * @return int
+     */
+     public int getValue()
+    {
+        return value;
+    }
+
+    /** Method prints the face value of the dice
+     *  @param  no params
+     *  @return String
+     */
+    public String toString()
+    {
+        return "Die" + name +" is showing " + value;
+    }
+}
---
+++ DieRunner.java
@@ -0,0 +1,171 @@
+
+import java.util.Scanner;
+/**
+ * Write a description of class DieRunner here.
+ *
+ * @author (Mark Vasfeld)
+ * @version (1.0 or Sep 23, 2018)
+ */
+public class DieRunner
+{
+     public static void main (String [] args)
+
+    {
+        System.out.println("\n\n\nStarting a new Game");
+        Die die1 = new Die("1");
+        Die die2 = new Die("2");
+        int initialRoll=0;
+        int score=0;
+        int numIncorrect=0;
+        int currentRoll=0;
+        int lastRoll=0;
+        Scanner scan = new Scanner(System.in);
+        
+        initialRoll = die1.getValue() + die2.getValue();
+        currentRoll = initialRoll;
+        lastRoll = initialRoll;
+        
+        System.out.println(die1);
+        System.out.println(die2);
+        System.out.println("Your initial roll is a(n):" + initialRoll);
+        System.out.println();
+        
+         while(numIncorrect<3)
+        {
+            System.out.println("Do you think your next roll will be higher,");
+            System.out.println("lower,or the same as this roll?(h/l/s)");
+            String answer = scan.next();
+            
+            switch (answer)
+            {
+                case "l":
+                   // System.out.println("You entered l");
+                    
+                    //Rolling dice
+                    die1.roll();
+                    die2.roll();
+                    
+                    //Printing face values of each die
+                    System.out.println(die1);
+                    System.out.println(die2);
+                    
+                    //Calculating current roll
+                    currentRoll = die1.getValue() + die2.getValue();
+                    
+                    //Printing total value of current roll
+                    System.out.println("Your new roll is a(n):" + currentRoll);
+                    
+                    //Compare last roll to current roll
+                    if (currentRoll < lastRoll )
+                    {
+                        score=score+1;
+                        System.out.println("Correct!");
+                    }
+                    else
+                    {
+                        numIncorrect=numIncorrect+1;
+                        System.out.println("Sorry,that is not correct");
+                    }  
+                    //Printing your Score
+                    System.out.println();
+                    System.out.println("Score:" + score + "     Incorrect Guesses:" + numIncorrect);
+
+                    //Printing total value of the last roll
+                    System.out.println("Last roll was:" + lastRoll);
+                    System.out.println();
+                    
+                    //Setting last roll to current roll
+                    lastRoll=currentRoll;
+                    
+                    break;
+                    
+                case "h":
+                    //System.out.println("You entered h");
+                    //Rolling dice
+                    die1.roll();
+                    die2.roll();
+                    
+                    //Printing face values of each die
+                    System.out.println(die1);
+                    System.out.println(die2);
+                    
+                    //Calculating current roll
+                    currentRoll = die1.getValue() + die2.getValue();
+                    
+                    //Printing total value of current roll
+                    System.out.println("Your new roll is a(n):" + currentRoll);
+                    
+                    //Compare last roll to current roll
+                    if (currentRoll > lastRoll )
+                    {
+                        score=score+1;
+                        System.out.println("Correct!");
+                    }
+                    else
+                    {
+                        numIncorrect=numIncorrect+1;
+                        System.out.println("Sorry,that is not correct");
+                    }  
+                    //Printing your Score
+                    System.out.println();
+                    System.out.println("Score:" + score + "     Incorrect Guesses:" + numIncorrect);
+
+                    //Printing total value of the last roll
+                    System.out.println("Last roll was:" + lastRoll);
+                    System.out.println();
+                    
+                    //Setting last roll to current roll
+                    lastRoll=currentRoll;
+                    
+                    break;
+                    
+                case "s":
+                    //System.out.println("You entered s");
+                    //Rolling dice
+                    die1.roll();
+                    die2.roll();
+                    
+                    //Printing face values of each die
+                    System.out.println(die1);
+                    System.out.println(die2);
+                    
+                    //Calculating current roll
+                    currentRoll = die1.getValue() + die2.getValue();
+                    
+                    //Printing total value of current roll
+                    System.out.println("Your new roll is a(n):" + currentRoll);
+                    
+                    //Compare last roll to current roll
+                    if (currentRoll == lastRoll )
+                    {
+                        score=score+1;
+                        System.out.println("Correct!");
+                    }
+                    else
+                    {
+                        numIncorrect=numIncorrect+1;
+                        System.out.println("Sorry,that is not correct");
+                    }  
+                    //Printing your Score
+                    System.out.println();
+                    System.out.println("Score:" + score + "     Incorrect Guesses:" + numIncorrect);
+
+                    //Printing total value of the last roll
+                    System.out.println("Last roll was:" + lastRoll);
+                    System.out.println();
+                    
+                    //Setting last roll to current roll
+                    lastRoll=currentRoll;
+                    
+                    break;
+                    
+                default:
+                    System.out.println();
+                    System.out.println("Please enter l for lower, h for higheer or s for same.");
+                    System.out.println();
+                    continue;
+            }                       
+        }
+        System.out.println("Game Over");
+    }
+}
